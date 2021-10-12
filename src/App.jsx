eca8c68df8b474c6f3927c47b69d5c4b4545f594
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-10-12 22:42:10 +0300

    recomit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import Circle from './components/Circle';
 import Square from './components/Square';
 
+const circleData = [
+  {
+    buttonText: 'spausti',
+    circleColor: 'red'
+  },
+  {
+    buttonText: 'traukti',
+    circleColor: 'pink'
+  }
+];
+
+const squareData = [
+  {
+    buttonText: 'spausti',
+    squareColor: 'blue'
+  },
+  {
+    buttonText: 'traukti',
+    squareColor: 'yellow'
+  }
+];
+
 function App() {
   return (
     <>
-      <Circle buttonText = {'spausti'} circleColor = {'red'}></Circle>
-      <Circle buttonText = {'traukti'} circleColor = {'pink'}></Circle>
+    {
+      circleData.map((circ, i) => <Circle buttonText = {circ.buttonText} circleColor = {circ.circleColor}></Circle>)
+    }
 
-      <Square buttonText = {'spausti'} squareColor = {'blue'}></Square>
-      <Square buttonText = {'traukti'} squareColor = {'yellow'}></Square>
+    {
+      squareData.map((sqrt, i) => <Square buttonText = {sqrt.buttonText} squareColor = {sqrt.squareColor}></Square>)
+    }
+    
+      {/* <Circle buttonText = {'spausti'} circleColor = {'red'}></Circle>
+      <Circle buttonText = {'traukti'} circleColor = {'pink'}></Circle> */}
+
+      {/* <Square buttonText = {'spausti'} squareColor = {'blue'}></Square>
+      <Square buttonText = {'traukti'} squareColor = {'yellow'}></Square> */}
 
     </>
   );
